b097270b0f60460bde00b742a346bee0b6f3803d
IdiotStinker <saltylightning564@gmail.com>
2025-03-01 08:30:02 -0800

    pysgh

--- exampleDirectoryProb.py
+++ exampleDirectoryProb.py
@@ -8,6 +8,7 @@ with open("input.txt") as f:
 startTime = datetime.strptime(start, "%Y-%m-%d")
 endTime = datetime.strptime(end, "%Y-%m-%d")
 
+#Just files for prob
 files = os.listdir("exampleFiles")
 
 transactions = []
@@ -17,6 +18,7 @@ for file in files:
     fileTime = datetime.strptime(file.split(".")[0], "%Y-%m-%d")
 
     if startTime <= fileTime <= endTime:
+        #just files
         filePath = os.path.join("exampleFiles", file)
 
         with open(filePath) as f:
