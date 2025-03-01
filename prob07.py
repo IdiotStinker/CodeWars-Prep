45e9d211cee68eca019fb5a7f2b2cfb5b8bdf371
IdiotStinker <saltylightning564@gmail.com>
2025-03-01 09:07:21 -0800

    tyuhf

--- prob07.py
+++ prob07.py
@@ -1,3 +1,11 @@
 with open("input.txt","r") as f:
     file = f.read().strip()
     file = file.split()
+
+dicti = {}
+for num in "124567890":
+    dicti[num] = -1
+
+for num in file:
+    dicti[num]+=1
+
--- prob08.py
+++ prob08.py
@@ -1,3 +1,9 @@
 with open("input.txt","r") as f:
     file = f.read().strip()
     file = file.split()
+ct = 0
+for i in range(len(file[0])):
+    if file[0][i].lower() == file[1].lower():
+        ct = ct + 1
+        
+print(f"There are {ct} {file[1].upper}'s in {file[0]}")
